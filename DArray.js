3012f32f9876e433fe058207e88497da353be320
ErikMovsisyan <erik.movsisyan.2016@bk.ru>
2025-09-30 13:37:24 +0400

    Dynamic Array

---
+++ DArray.js
@@ -0,0 +1,128 @@
+class DArray {
+    #size = 0;
+    #capacity = 0;
+    #arr = null;
+    #CAP_EXPONENT = 2;
+
+    constructor(cap) {
+        if (cap <= 0) return;
+        this.#capacity = cap;
+        this.#arr = new Uint32Array(cap);
+    }
+    size() {
+        return this.#size;
+    }
+    capacity() {
+        return this.#capacity;
+    }
+    empty() {
+        if (this.#size === 0) {
+            return true;
+        }
+        return false;
+    }
+    reserve(n) {
+        if (n > this.#capacity) {
+            this.resize(n);
+        }
+    }
+    clear() {
+        this.#size = 0;
+    }
+    set(index, value) {
+        this.#arr[index] = value;
+    }
+    front() {
+        return this.#arr[0];
+    }
+    back() {
+        this.#arr[this.size - 1];
+    }
+    toArray() {
+        return this.#arr.slice(0,this.#size - 1)
+    }
+    popBack() {
+        if (this.#size === 0) {
+            throw new Error("out of range")
+        }
+
+        this.#size--;
+    }
+    insert (pos,value) {
+        if (pos < 0 || pos >= this.#size) {
+            throw new Error("Out of range");
+        }
+         if (this.#size === this.#capacity) {
+              this.resize(this.#capacity * this.#CAP_EXPONENT);
+    }
+        for (let i = this.#size - 1; i >= pos; --i) {
+               this.#arr[i + 1] = this.#arr[i];
+        }
+    this.#arr[pos] = value;
+    this.#size++;
+  }
+
+    resize (new_cap, fill = 0) {
+        const tmp = new Uint32Array(new_cap);
+        for (let i = 0; i < this.#size; ++i) {
+            tmp[i] = this.#arr[i]; 
+        }
+        for (let i = this.#size; i < new_cap; ++i) {
+            tmp[i] = fill;
+        }
+        this.#capacity = new_cap;
+        this.#arr = tmp;
+    }
+    push_back(elem) {
+        if (this.#size === this.#capacity) {
+            this.resize(this.#size * this.#CAP_EXPONENT);
+        }
+        this.#arr[this.#size++] = elem;
+    }
+    at(index) {
+        if (index < 0 || index >= this.#size) {
+            throw new Error("Out of range");
+        }
+        return this.#arr[index];
+    }
+    erase(pos) {
+        if (pos < 0 || pos >= this.#size) {
+            throw new Error("Out of range");
+        }
+        for (let i = pos; i < this.#size - 1; ++i) {
+            this.#arr[i] = this.#arr[i + 1];
+        }
+        this.#size--;
+    }
+    [Symbol.iterator] () {
+        const collection = this.#arr;
+        const colection_length = this.#size;
+        let index = 0;
+        return {
+            next() {
+                if (index < colection_length) {
+                    return {value : collection[index++], done : false}
+                }
+                return {value : undefined, done : true}
+            }
+        }
+    }
+}
+let arr = new DArray(4)
+arr.push_back(3);
+arr.push_back(5);
+arr.push_back(6);
+arr.push_back(7);
+console.log(...arr);
+
+
+arr.popBack()
+console.log(...arr);
+
+arr.insert(2,15);
+console.log(...arr);
+
+
+
+
+
