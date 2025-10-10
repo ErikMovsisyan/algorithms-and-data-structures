511e685a1fafdb3eaf773950193fd795509142f0
ErikMovsisyan <erik.movsisyan.2016@bk.ru>
2025-10-10 15:09:30 +0400

    Data Structure

---
+++ queue.js
@@ -0,0 +1,36 @@
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+    enqueue(value) {
+        this.items.push(value);
+    }
+    dequeue() {
+        if (this.isEmpty()) throw new Error("Queue is empty");
+        return this.items.shift();
+    }
+    front() {
+        if (this.isEmpty()) throw new Error("Queue is empty");
+        return this.items[0];
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+    size() {
+        return this.items.length;
+    }
+    clear() {
+        this.items = [];
+    }
+}
+
+const queue = new Queue();
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+console.log(queue.front());
+console.log(queue.dequeue());
+console.log(queue.size());
+console.log(queue.isEmpty());
+queue.clear();
+console.log(queue.isEmpty());
